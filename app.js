dff8499aec9939eae587860caf7ea43c89587c06
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-01 19:24:54 -0500

    cleared and readded a dynamic grid to the main portion of our application

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const resetBtn = document.querySelector('#reset');
 const boxes = document.querySelectorAll('.box');
 const gridSizeForm = document.querySelector('#grid-size');
+const main = document.querySelector('main');
 
 //eventlisteners
 boxes.forEach(function(box, index){
@@ -27,5 +28,21 @@ function clearGrid(e){
 function changeGrid(e){
     e.preventDefault();
     const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
-    console.log(e);
+    const grid = makeGrid(gridSizeValue);
+    //set the main to the grid
+    main.innerHTML = ''; // this clears the main area
+    main.appendChild(grid);
+
+}
+
+
+function makeGrid(size){
+    const element = document.createElement('h1');
+    element.textContent = 'GRID!!!!!!!!!';
+    return element;
+     //make the grid
+        //make a div with class of row
+        //make a div with class col-md-12 inside of row
+        //make 6 divs with class of box inside of col-md-12
+        //copy and pasted 6 times
 }
